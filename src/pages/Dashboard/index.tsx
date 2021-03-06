8c3e6ef20d8a3cdbef67d1fde29092cdd7367920
Pedro Araujo <pedro-foa@hotmail.com>
2021-03-06 20:57:00 -0300

    :sparkles: Create Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import Loading from 'react-loader-spinner';
 
-// import { Container } from './styles';
+import { FiSearch } from 'react-icons/fi';
+import { UserContext } from '../../hooks/user';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import ListItem from '../../components/ListItem';
+import UserInfo from '../../components/UserInfo';
+
+import { Title, Form, Repositories, Content } from './styles';
+import Error from '../../components/Error';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const {
+    userInfo,
+    repositories,
+    isLoading,
+    errorMessage,
+    setUserData,
+  } = useContext(UserContext);
+
+  const [user, setUser] = useState('');
+
+  const shouldShowUserInfo = !!userInfo.name && !isLoading && !errorMessage;
+
+  return (
+    <>
+      <Title>Explore repositórios no GitHub</Title>
+      <Form onSubmit={e => setUserData(user, e)}>
+        <Input
+          placeholder="Digite o nome do usuário"
+          type="text"
+          onChange={e => setUser(e.target.value)}
+        />
+        <Button type="submit">
+          <FiSearch size={30} />
+        </Button>
+      </Form>
+      <Content>
+        {errorMessage && <Error message={errorMessage} />}
+        {isLoading && (
+          <Loading type="ThreeDots" height={60} width={60} color="#04d361" />
+        )}
+        {shouldShowUserInfo && (
+          <>
+            <UserInfo {...userInfo} />
+            <Repositories>
+              {repositories.map(
+                ({ id, name, description, stargazers_count }) => (
+                  <ListItem
+                    key={id}
+                    id={id}
+                    title={name}
+                    subtitle={description}
+                    stargazers={stargazers_count}
+                  />
+                ),
+              )}
+            </Repositories>
+          </>
+        )}
+      </Content>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,29 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  line-height: 56px;
+  max-width: 450px;
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+`;
+
+export const Content = styled.div`
+  margin-top: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 700px;
+  width: 100%;
+`;
+
+export const Repositories = styled.div`
+  margin-top: 30px;
+`;
