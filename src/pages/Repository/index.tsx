fc5670e2a7c6592bbb33722779c3e6a5cf89a251
Pedro Araujo <pedro-foa@hotmail.com>
2021-03-08 19:11:10 -0300

    :sparkles: Add Repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { FiChevronLeft } from 'react-icons/fi';
+import { Link, useRouteMatch } from 'react-router-dom';
+import Button from '../../components/Button';
+import Error from '../../components/Error';
 
-// import { Container } from './styles';
+import { UserContext } from '../../hooks/user';
+
+import { Container, RepositoryInfo } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository = () => {
-  return <div>Repository</div>;
+  const { userInfo, repositories } = useContext(UserContext);
+
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  const repository = repositories.find(
+    repo => repo.id === Number(params.repository),
+  );
+
+  return (
+    <Container>
+      <Link to="/">
+        <FiChevronLeft size={16} />
+        Voltar
+      </Link>
+
+      {repository ? (
+        <>
+          <RepositoryInfo>
+            <header>
+              <img src={userInfo.avatar_url} alt={userInfo.name} />
+              <div>
+                <strong>{repository.name}</strong>
+                <p>{repository.description}</p>
+              </div>
+            </header>
+            <ul>
+              <li>
+                <strong>{repository.stargazers_count}</strong>
+                <span>Stars</span>
+              </li>
+              <li>
+                <strong>{repository.language}</strong>
+                <span>Language</span>
+              </li>
+              <li>
+                <strong>{repository.forks}</strong>
+                <span>Forks</span>
+              </li>
+            </ul>
+
+            <a
+              target="_blank"
+              href={repository.html_url}
+              rel="noopener noreferrer"
+            >
+              <Button>Ir para o repositório</Button>
+            </a>
+          </RepositoryInfo>
+        </>
+      ) : (
+        <Error message="Repositório não encontrado" />
+      )}
+    </Container>
+  );
 };
 
 export default Repository;
--- src/pages/Repository/styles.ts
+++ src/pages/Repository/styles.ts
@@ -1,3 +1,83 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color 0.2s;
+    margin-bottom: 30px;
+
+    &:hover {
+      color: #666;
+    }
+
+    svg {
+      margin-right: 5px;
+    }
+  }
+`;
+
+export const RepositoryInfo = styled.section`
+  margin-top: 80px;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 30px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #737380;
+        margin-top: 4px;
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin-top: 40px;
+
+    li {
+      & + li {
+        margin-left: 20px;
+      }
+
+      strong {
+        display: block;
+        font-size: 25px;
+
+        color: #3d3d4d;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #6c6c80;
+      }
+    }
+  }
+
+  a {
+    margin-top: 40px;
+
+    button {
+      width: 100%;
+    }
+  }
+`;
